b30a280ec53ab354d1c26c8b76676c03d1127e66
roseweigee <geegeeoffice@gmail.com>
2026-04-07 06:29:17 +0800

    @EnableJpaRepositories(basePackages = {     "com.demo.order.repository",     "com.demo.order.saga"  // ← 加這行 })

--- order-service/src/main/java/com/demo/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/demo/order/OrderServiceApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {
+        "com.demo.order.repository",
+        "com.demo.order.saga"  // ← 加這行
+})
 public class OrderServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
